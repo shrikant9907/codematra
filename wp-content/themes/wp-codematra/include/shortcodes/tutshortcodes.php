5501b3eb876e1b661590677a0d9f6be4c9e13281
Shrikant Yadav <shrikant9907@gmail.com>
2021-09-25 15:12:59 +0530

    popular posts

--- wp-content/themes/wp-codematra/include/shortcodes/tutshortcodes.php
+++ wp-content/themes/wp-codematra/include/shortcodes/tutshortcodes.php
@@ -135,6 +135,51 @@ function categories_list($atts) {
         $output .= '</li>';
     }
     $output .= '</ul>';
-    echo $output;
+    return $output;
+  }
+}
+
+/*
+* Show Popular Posts with visit counts
+*/
+add_shortcode('popularPostsList' ,'popular_posts_listing');
+function popular_posts_listing($atts) {
+
+  extract(shortcode_atts(array(
+    'show_counts' => true,
+    'show_heading'    => true,
+    'post_type'       => 'post',
+    'heading'         => 'Popular Posts',
+    'posts_per_page'  => 5,
+  ), $atts));
+
+  $posts = get_posts(array(
+    'post_type'       => $post_type,
+    'posts_per_page'  => $posts_per_page,
+    'meta_key'        => 'visit_counts', 
+    'order'           => 'DESC',
+    'orderby'         => 'meta_value_num',
+  ));
+  
+  if ($show_heading) {
+    $output = '<h2 class="f20 font_bold text-primary">'.$heading.'</h2>';
+  } else {
+    $output = '';
+  }
+  if ( ! empty( $posts ) ) {
+    $output .= '<ul>';
+    foreach( $posts as $post ) {
+      $postID = $post->ID;
+      $postTitle = $post->post_title;
+      $visitsCount = get_post_meta($postID, 'visit_counts', true);
+      $output .= '<li class="relative d-flex align-items-center justify-content-between" >';
+      $output .= '<a class="text-dark" href="' . esc_url( get_permalink( $postID ) ) . '" >' . esc_html( $postTitle ) . '</a> ';
+      if ($show_counts) {
+        $output .= '<span class="badge bg-primary static"><i class="fas fa-eye mr_5"></i>'.$visitsCount.'</span>';
+      }
+      $output .= '</li>';
+    }
+    $output .= '</ul>';
+    return $output;
   }
 }
--- wp-content/themes/wp-codematra/include/theme-default-functions.php
+++ wp-content/themes/wp-codematra/include/theme-default-functions.php
@@ -415,3 +415,26 @@ function getBadgeColor($name) {
 
     return $badgeColor;
 }
+
+/*
+ * Set visits counts on posts, pages and custom post type.
+ */
+add_action('wp_head', 'setVistsCounts');
+function setVistsCounts() {
+  global $post;
+  $postID = $post->ID;
+  if ($postID):
+    $visitKey = 'visit_counts';
+    $visitCounts = get_post_meta($postID, $visitKey, true);
+    
+    if ($visitCounts != '' && $visitCounts >= 0):
+      $visitCounts++;
+      update_post_meta($postID, $visitKey, $visitCounts);
+    else:
+      $visitCounts = 1;
+      delete_post_meta($postID, $visitKey);
+      add_post_meta($postID, $visitKey, $visitCounts);
+    endif;
+    
+  endif;
+}
