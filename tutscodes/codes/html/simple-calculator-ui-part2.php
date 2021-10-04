362de6947d2cf6d3b57ad60bf19ac31a48478da2
Shrikant Yadav <shrikant9907@gmail.com>
2021-10-04 13:55:32 +0530

    calculator code

---
+++ tutscodes/codes/html/simple-calculator-ui-part2.php
@@ -0,0 +1,28 @@
+<!-- Calculator Layout UI Start by https://codematra.com/ -->
+<div class="code-matra-calculator-ui">  
+
+  <!-- Calculator Header -->
+  <div class="cm-header">
+    <h3>Calculator <span>by <a target="_blank" href="https://codematra.com/how-to-create-a-simple-calculator-in-html">Code Matra</a></span> <span title="close" class="cm-close">&times;</span></h3>
+  </div>
+
+  <!-- Calculator Field -->
+  <div class="cm-field">
+    <input class="form-control" type="text" readonly name="input" value="0" />
+  </div>
+
+  <!-- Calculator Keys -->
+  <div class="cm-keys">
+    <form action="">
+      <div class="cm-row"><button type="button" class="btn">C</button><button type="button" class="btn"></button><button type="button" class="btn"></button><button type="button" class="btn">/</button></div>
+      <div class="cm-row"><button type="button" class="btn">7</button><button type="button" class="btn">8</button><button type="button" class="btn">9</button><button type="button" class="btn">&times;</button></div>
+      <div class="cm-row"><button type="button" class="btn">4</button><button type="button" class="btn">5</button><button type="button" class="btn">6</button><button type="button" class="btn">-</button></div>
+      <div class="cm-row"><button type="button" class="btn">1</button><button type="button" class="btn">2</button><button type="button" class="btn">3</button><button type="button" class="btn">+</button></div>
+      <div class="cm-row"><button type="button" class="btn">C</button><button type="button" class="btn">0</button><button type="button" class="btn">.</button><button type="submit" class="btn btn-secondary">=</button></div>
+    </form>
+  </div>
+
+  <!-- Site Attributes -->
+  <div class="siteurl">https://codematra.com</div>
+</div>
+<!-- Calculator Layout UI End -->
---
+++ tutscodes/codes/html/simple-calculator-ui-part3.php
@@ -0,0 +1,117 @@
+<!-- Calculator Styles -->
+<style>
+
+  /* Calculator Container UI - https://codematra.com/*/
+  .code-matra-calculator-ui {
+    font-family: Roboto;
+    font-size: 18px;
+    color: #666666;
+    max-width: 300px;
+    box-shadow: 2px 2px 9px 5px #f5f5f5 !important;
+    border: 1px solid #f5f5f5;
+    margin: 30px auto;
+    padding: 20px 20px;
+    box-sizing: border-box;
+  }
+
+  /* Calculator Header UI */
+  .code-matra-calculator-ui .cm-header h3 {
+    font-size: 16px;
+    background-color: #117CA6;
+    text-transform: uppercase;
+    color: #ffffff;
+    margin: -20px -20px 20px -20px;
+    padding: 15px 20px;
+    letter-spacing: 1px;
+    position: relative;
+  }
+
+  .code-matra-calculator-ui .cm-header span {
+    font-size: 10px;
+  }
+
+  .code-matra-calculator-ui .cm-header span a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  .code-matra-calculator-ui .cm-header .cm-close {
+    right: -14px;
+    top: -15px;
+    font-size: 16px;
+    position: absolute;
+    background: #fff;
+    border-radius: 100%;
+    color: #000;
+    width: 30px;
+    height: 30px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0px 0px 8px 0px #a8a6a6 !important;
+  }
+
+  /* Calculator Field UI */
+  .code-matra-calculator-ui .cm-field input{
+    width: 100%;
+    border: 2px solid #efefef !important;
+    margin-bottom: 20px;
+    height: 45px;
+    font-family: 'Orbitron', sans-serif;
+    padding: 5px 10px;
+    font-size: 23px;
+    letter-spacing: 2px;
+    text-align: right;
+    outline: unset !important;
+    pointer-events: none; 
+    cursor: pointer;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none; 
+    -ms-user-select: none;
+    user-select: none; 
+  }
+
+  /* Calculator Body - Buttons UI */
+  .code-matra-calculator-ui .cm-row {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .code-matra-calculator-ui .cm-row .btn {
+    width: 25%;
+    font-family: 'Orbitron', sans-serif;
+    border-radius: 0;
+    border: 1px solid #efefef;
+    font-size: 20px;
+    height: 45px;
+    background-color: #ffffff;
+    color: #666666;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none; 
+    -ms-user-select: none;
+    user-select: none; 
+  }
+
+  .code-matra-calculator-ui .cm-row .btn:hover {
+    background-color: #117CA6;
+    border-color: #117CA6;
+    color: #ffffff;
+  }
+
+  .code-matra-calculator-ui .cm-row button[type="submit"].btn {
+    background-color: #f34423;
+    border-color: #f34423;
+    color: #ffffff;
+  }
+
+  /* Site URL */
+  .code-matra-calculator-ui .siteurl {
+    font-size: 10px;
+    color: #cccccc;
+  }
+</style>
