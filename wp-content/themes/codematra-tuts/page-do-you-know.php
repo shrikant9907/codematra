d4ad19ab555f735305f81e8b9d9221f58cbe19ad
Shrikant Yadav <shrikant9907@gmail.com>
2021-10-11 11:42:56 +0530

    fixes

--- wp-content/themes/codematra-tuts/page-do-you-know.php
+++ wp-content/themes/codematra-tuts/page-do-you-know.php
@@ -18,7 +18,7 @@
         </div>
         <div class="row">
           <?php
-          if ($handle = opendir('./tuts-images/doyouknow')) {
+          if ($handle = opendir(ABSPATH.'/tuts-images/doyouknow')) {
             while (false !== ($file = readdir($handle))) {
               if ($file != "." && $file != "..") {
                 $name = ucfirst(str_replace('-', ' ', basename($file, '.png')));
@@ -26,16 +26,19 @@
                 <div class="col-12 col-md-6">
                   <div class="card cui3 r_0 doyouknow mb_20">
                     <img src="<?php echo site_url('/tuts-images/doyouknow/'. $file); ?>" alt="<?php echo $name; ?>" />
-                    <div class="card-body">
-                      <p>Share on social media...</p>
-                      <ul class="socialicons siconsb"> 
-                        <li class="facebook"><a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fcodematra&quote=<?php echo $name; ?>" title="Facebook" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-                        <li class="linkedin"><a href="http://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fwww.facebook.com%2Fcodematra&title=<?php echo $name; ?>&summary=Do you know - Tips and Tricks from beginners and developers.&source=https%3A%2F%2Fwww.facebook.com%2Fcodematra" title="Linked In" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-                        <li class="mailto"><a href="mailto:?subject=<?php echo $name; ?>&body=Do you know - Tips and Tricks from beginners and developers.:%20https%3A%2F%2Fwww.facebook.com%2Fcodematra" title="Mail To" target="_blank"><i class="far fa-envelope"></i></a></li>
-                        <!-- <li class="twitter"><a href="#" title="Twitter" target="_blank"><i class="fab fa-twitter"></i></a></li> -->
-                        <!-- <li class="instagram"><a href="#" title="Instagram" target="_blank"><i class="fab fa-instagram"></i></a></li> -->
-                        <!-- <li class="youtube"><a href="#" title="Youtube" target="_blank"><i class="fab fa-youtube"></i></a></li> -->
-                      </ul>
+                    <div class="card-body p-2 text-center d-flex justify-content-center align-items-center">
+                      <div class="sharecard">
+                        <p class="mb-0">Share on social media...</p>
+                        <ul class="socialicons siconsb mx-auto"> 
+                          <li class="facebook"><a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fcodematra&quote=<?php echo $name; ?>" title="Facebook" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                          <li class="linkedin"><a href="http://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fwww.facebook.com%2Fcodematra&title=<?php echo $name; ?>&summary=Do you know - Tips and Tricks from beginners and developers.&source=https%3A%2F%2Fwww.facebook.com%2Fcodematra" title="Linked In" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                          <li class="mailto"><a href="mailto:?subject=<?php echo $name; ?>&body=Do you know - Tips and Tricks from beginners and developers.:%20https%3A%2F%2Fwww.facebook.com%2Fcodematra" title="Mail To" target="_blank"><i class="far fa-envelope"></i></a></li>
+                          <li class="whatsapp"><a href="https://api.whatsapp.com/send?text=<?php the_permalink(); ?>" title="Whats App" target="_blank"><i class="fab fa-whatsapp"></i></a></li>
+                          <!-- <li class="twitter"><a href="#" title="Twitter" target="_blank"><i class="fab fa-twitter"></i></a></li> -->
+                          <!-- <li class="instagram"><a href="#" title="Instagram" target="_blank"><i class="fab fa-instagram"></i></a></li> -->
+                          <!-- <li class="youtube"><a href="#" title="Youtube" target="_blank"><i class="fab fa-youtube"></i></a></li> -->
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
