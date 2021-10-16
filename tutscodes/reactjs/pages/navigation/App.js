85840c33815d666fdd76163e319e0fe895cfdde4
Shrikant Yadav <shrikant9907@gmail.com>
2021-10-16 17:47:46 +0530

    date picker

---
+++ tutscodes/reactjs/pages/navigation/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import AboutPage from "./components/pages/AboutPage";
+import ContactPage from "./components/pages/ContactPage";
+// import './components/pages/PageStyle.scss';
+
+function App() { 
+  return (
+    <Router>
+
+      <nav className="navui1">
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About Us</Link></li>
+          <li><Link to='/contact'>Contact Us</Link></li>
+        </ul>
+      </nav>
+
+      <div className="pagewr">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage}/>
+        </Switch>
+      </div>
+
+    </Router>
+  );
+}
+
+export default App;
---
+++ tutscodes/reactjs/pages/reactdatepicker/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import ReactDatePicker from "./components/pages/ReactDatePicker";
+import './components/pages/PageStyle.scss'; // It is for nav bar style
+
+function App() { 
+  return (
+    <Router>
+
+      <nav className="navui1">
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/datepicker'>React Datepicker</Link></li>
+        </ul>
+      </nav>
+
+      <div className="pagewr">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/datepicker" component={ReactDatePicker}/>
+        </Switch>
+      </div>
+
+    </Router>
+  );
+}
+export default App;
