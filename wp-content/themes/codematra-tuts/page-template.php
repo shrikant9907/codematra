a23f4f6a1a76f95228872ae3188e01c21ad8279e
Shrikant Yadav <shrikant9907@gmail.com>
2021-10-19 12:47:35 +0530

    template page updates

--- wp-content/themes/codematra-tuts/page-template.php
+++ wp-content/themes/codematra-tuts/page-template.php
@@ -10,25 +10,40 @@ $visitsCount = get_post_meta(get_the_ID(), 'visit_counts', true);
 if (!$visitsCount) {
   $visitsCount = 0;
 }
+
+$downloadCount = get_post_meta(get_the_ID(), 'download_count', true);
+if (!$downloadCount) {
+  $downloadCount = 0;
+}
 ?>
 <h1 class="text-center mb_0 text-secondary border-top ptb_40 border-bottom mont_serrat f30 lh32"><?php the_title(); ?></h1>
 <section class="common-section-ui page-breadcrumb border-bottom pti_10 pbi_10">
   <div class="container">
-    <p class="text-muted f14 m-0"><a class="tdn text-secondary" href="<?php echo site_url(); ?>" class="text-muted">Home</a> / <?php the_title(); ?></p>
+    <p class="text-muted f14 m-0"><a class="tdn text-secondary" href="<?php echo site_url(); ?>" class="text-muted">Home</a> / <a class="tdn text-secondary" href="<?php echo site_url('/templates'); ?>" class="text-muted">Templates</a> / <?php the_title(); ?></p>
   </div>
 </section>
-<section class="page-section common-section-ui">
+<section class="page-section common-section-ui pt_40">
   <div class="container">
     <div class="row">
       <div class="col-12 col-md-8">
-        <?php if(has_post_thumbnail()) { ?>
-          <img class="card-img-top r_0 mb_20 border" src="<?php echo $image; ?>" alt="<?php the_title(); ?>" />
-        <?php } ?>
-        <p class="text-muted"><i class="fa fa-eye mr_5 text-primary" title="Views" aria-hidden="true"></i> Views: <?php echo $visitsCount; ?></p>
+        <div class="template-preview-card">
+          <div class="overlay overlay_b absolute d-none">
+            <a class="btn btn-outline-secondary" href="https://shrikant9907.github.io/blog-template/dist/" target="_blank" rel="noopener">
+              <i class="fas fa-link mr_10"></i>Launch Live Preview
+            </a>
+          </div>
+          <?php if(has_post_thumbnail()) { ?>
+            <img src="<?php echo $image; ?>" alt="<?php the_title(); ?>" />
+          <?php } ?>
+        </div>
+        <p class="text-muted d-flex justify-content-between">
+          <span><i class="fa fa-eye mr_5 text-primary" title="Views" aria-hidden="true"></i><?php echo $visitsCount; ?></span>
+          <span><i class="fa fa-download mr_5 text-primary" title="Downloads" aria-hidden="true"></i> <?php echo $downloadCount; ?></span>
+        </p>
         <?php the_content(); ?>
       </div>
       <div class="col-12 col-md-4">
-        <?php get_sidebar(); ?>
+        <?php get_sidebar('template'); ?>
       </div>
     </div>
   </div>
---
+++ wp-content/themes/codematra-tuts/sidebar-template.php
@@ -0,0 +1,35 @@
+
+<a class="trackdownloads f20 font_bold btn btn-secondary hs_11 w-100 btnui3s mb_30" href="https://github.com/shrikant9907/cm-blog-template-code-matra/archive/refs/heads/main.zip" target="_blank" rel="noopener">
+<i class="fas fa-download mr_10"></i>Free Download
+</a>
+
+<!-- About Template -->
+<div class="card cui2 noshadow cteam py_0 px_0 mb_20 f14 r_0 border">
+  <h3 class="card-header rounded-0 py_15 px_15 border-bottom text-uppercase  text-primary f16 mb_0 font_bold">Template Details</h3>
+  <div class="cbody p-0">
+    <ul class="listing type2">
+      <li class="list-item d-flex justify-content-between">
+        <span>Template Name</span><span>CM Blog Template</span>
+      </li>
+      <li class="list-item d-flex justify-content-between">
+        <span>License</span><span>MIT License</span>
+      </li>
+      <li class="list-item d-flex justify-content-between">
+        <span>Version</span><span>1.0.0</span>
+      </li>
+      <li class="list-item d-flex justify-content-between">
+        <span>Bootstrap Version</span><span>5.1.0</span>
+      </li>
+      <li class="list-item d-flex justify-content-between">
+        <span>Released</span><span>OCT, 19, 2021</span>
+      </li>
+      <li class="list-item d-flex justify-content-between">
+        <span>Last Update</span><span>OCT, 19, 2021</span>
+      </li>
+      <li class="list-item d-flex justify-content-between">
+        <span>Downloads</span><span>84</span>
+      </li>
+      
+    </ul>
+  </div>
+</div>
