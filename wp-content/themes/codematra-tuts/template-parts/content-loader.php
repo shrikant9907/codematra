2e8085547fe8df2491ee2831f9af7ac9a35ebe20
Shrikant Yadav <shrikant9907@gmail.com>
2021-10-28 22:54:18 +0530

    fixes

--- wp-content/themes/codematra-tuts/template-parts/content-loader.php
+++ wp-content/themes/codematra-tuts/template-parts/content-loader.php
@@ -1,6 +1,6 @@
 <div class="site-loader overlay_w flex justify-content-center align-items-center">
   <div class="cbody">
-    <div class="w_200 mx-auto mb_20"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/site-logo.png" class="card-img"></div>
+    <!-- <div class="w_200 mx-auto mb_20"><img src="<?php //echo get_stylesheet_directory_uri(); ?>/images/site-logo.png" class="card-img"></div> -->
     <div class="loader f50 loader-col static">
       <i class="fas fa-spinner fa-pulse mb_15"></i>
       <span class="loader-text f16">Loading...</span>
