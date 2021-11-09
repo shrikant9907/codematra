687c54ff37df866c28ca652997e1972cbfd501b3
Shrikant Yadav <shrikant9907@gmail.com>
2021-11-09 14:26:12 +0530

    updated

--- wp-content/themes/codematra-tuts/template-parts/content-loader.php
+++ wp-content/themes/codematra-tuts/template-parts/content-loader.php
@@ -2,7 +2,7 @@
   <div class="cbody">
     <!-- <div class="w_200 mx-auto mb_20"><img src="<?php //echo get_stylesheet_directory_uri(); ?>/images/site-logo.png" class="card-img"></div> -->
     <div class="loader f50 loader-col static">
-      <i class="fas fa-spinner fa-pulse mb_15"></i>
+      <i class="fas fa-spinner text-primary fa-pulse mb_15"></i>
       <span class="loader-text f16">Loading...</span>
     </div>
   </div>
